f468e3d570006b1e7dfc450ed2fa684e3e1a06f2
Daniel An <mkandaniel@gmail.com>
2021-11-22 13:19:15 -0800

    add the component Navbar and staged some components in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import Container from 'react-bootstrap/Container'
+import Navbar from '../components/NavBar';
+import { Link, Route, Navigate } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-
+      <header>
+        <Navbar />
+      </header>
+      <Container>
+        {/* Routes will go here */}
+      </Container>
     </div>
   );
 }
