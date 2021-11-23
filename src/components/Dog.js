4bde7b22a05ce3427a6ffa3c1d6b8e441b40bac5
Daniel An <mkandaniel@gmail.com>
2021-11-22 21:23:36 -0800

    Staged Owner homepage with some of its components

---
+++ src/components/Dog.js
@@ -0,0 +1,13 @@
+import Col from 'react-bootstrap/Col'
+
+const Dog = () => {
+
+    return(
+        <Col>Doggo placeholder</Col>
+    )
+}
+
+export default Dog
+
+{/* The plan is for this to be like single dog cards, or lists, or pictures, of the owners dogs
+That is why it is returning multiple Cols, and here we will also add in the handle functions for the dogs */}
