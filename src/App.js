540e2f50f121e5bb2d97dd2533eff32f139328c8
alyshia bentley <alyshiabentley@gmail.com>
2021-11-27 11:42:55 -0800

    add registration and user can successfully register

--- src/App.js
+++ src/App.js
@@ -7,26 +7,33 @@ import WalkerHome from "./components/WalkerHome";
 import Welcome from './components/Welcome'
 import Container from "react-bootstrap/Container";
 import {useState} from 'react'
+import Register from "./components/Register";
 
 function App() {
   const [username, setUsername] = useState()
 
   return (
-    <div className="App">
-      <header>
-        <Navigation setUsername={setUsername}/>
-      </header>
-      <Welcome />
-      <Routes>
-        <Route path="/owner" element={<OwnerHome />}>
-          {/* <Route path='/edit' element={<Edit />} /> */}
-        </Route>
-        <Route path="/walker" element={<WalkerHome />}>
-          {/* <Route path='/edit' element={<Edit />} /> */}
-        </Route>
-      </Routes>
-    </div>
-  );
+		<div className='App'>
+			<header>
+				<Navigation setUsername={setUsername} />
+			</header>
+			{/* <Welcome /> */}
+			<Routes>
+				<Route path='/' element={<Welcome />}>
+					{/* <Route path='/edit' element={<Edit />} /> */}
+				</Route>
+				<Route path='/owner' element={<OwnerHome />}>
+					{/* <Route path='/edit' element={<Edit />} /> */}
+				</Route>
+				<Route path='/walker' element={<WalkerHome />}>
+					{/* <Route path='/edit' element={<Edit />} /> */}
+				</Route>
+				<Route path='/register' element={<Register />}>
+					{/* <Route path='/edit' element={<Edit />} /> */}
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Register.js
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const Register = () => {
+	const ownerApiCall = 'http://localhost:4000/owner/register/';
+    const walkerApiCall = 'http://localhost:4000/walker/register/';
+
+	// set information for the new uesr
+	const [user, setUser] = useState({
+		name: '',
+		city: '',
+		username: '',
+		password: '',
+	});
+
+	//set user type for api call
+	const [userType, setUserType] = useState('');
+
+	//update the user's information 
+	const handleChange = (event) => {
+		setUser({ ...user, [event.target.name]: event.target.value });
+	};
+
+	// select dog owner or dog walker 
+	const onSelected = (event) => {
+		setUserType(event.target.value);
+	};
+
+	// submit registration
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		// if they are an owner
+		if (userType === 'owner') {
+			fetch(ownerApiCall, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(user),
+			}).then(() =>
+				setUser({ name: '', city: '', username: '', password: '' })
+			);
+            console.log( "new owner created")
+		}
+		// if they are a walker
+        else if (userType === 'walker')
+        {
+            fetch(walkerApiCall, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(user),
+			}).then(() =>
+				setUser({ name: '', city: '', username: '', password: '' })
+			);
+            console.log("new walker created")
+		}
+
+        
+	};
+
+	return (
+		<div>
+			<h1> Owner Registration</h1>
+			<Form onSubmit={handleSubmit}>
+				<Form.Group className='mb-3' controlId='formName'>
+					I am a....
+					<Form.Check
+						inline
+						label='Dog Owner'
+						name='user-type'
+						type='radio'
+						id={'owner-radio'}
+						value='owner'
+						onChange={onSelected}
+					/>
+					<Form.Check
+						inline
+						label='Dog Walker'
+						name='user-type'
+						type='radio'
+						id={'walker-radio'}
+						value='walker'
+						onChange={onSelected}
+					/>
+				</Form.Group>
+				<Form.Group className='mb-3' controlId='formName'>
+					<Form.Label> Name</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter name'
+						name='name'
+						value={user.name}
+						onChange={handleChange}
+					/>
+				</Form.Group>
+				<Form.Group className='mb-3' controlId='formCity'>
+					<Form.Label>City</Form.Label>
+					<Form.Control
+						type='text'
+						placeholder='Enter your city'
+						name='city'
+						value={user.city}
+						onChange={handleChange}
+					/>
+				</Form.Group>
+				<Form.Group className='mb-3' controlId='formEmail'>
+					<Form.Label>Email address</Form.Label>
+					<Form.Control
+						type='email'
+						placeholder='Enter email'
+						name='email'
+						value={user.email}
+						onChange={handleChange}
+					/>
+				</Form.Group>
+
+				<Form.Group className='mb-3' controlId='formBasicPassword'>
+					<Form.Label>Password</Form.Label>
+					<Form.Control
+						type='password'
+						placeholder='Password'
+						name='password'
+						value={user.password}
+						onChange={handleChange}
+					/>
+				</Form.Group>
+
+				<Button variant='primary' type='submit'>
+					Submit
+				</Button>
+			</Form>
+		</div>
+	);
+};
+
+export default Register;
