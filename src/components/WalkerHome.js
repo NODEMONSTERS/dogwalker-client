9fd5e9192ab79a97fe433fc5cb287a1ef02756e7
Daniel An <mkandaniel@gmail.com>
2021-11-29 13:28:44 -0800

    finished populating dogs into Cards

--- src/components/WalkerHome.js
+++ src/components/WalkerHome.js
@@ -1,28 +1,40 @@
-import {useEffect, useState} from 'react'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
+import { useEffect, useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const WalkerHome = () => {
-    const [dogs, setDogs] = useState()
+  const [dogs, setDogs] = useState();
 
-    useEffect(() => {
-        const makeApiCall = async () => {
-            const res = await fetch('https://localhost:4000/dogs')
-            const json = res.json()
-            setDogs(json)
-        }
-        makeApiCall();
-    }, [])
-    return(
-        <Container>
+  useEffect(() => {
+    const makeApiCall = async () => {
+      const res = await fetch("http://localhost:4000/dog");
+      const json = await res.json();
+      setDogs(json.data);
+    };
+    makeApiCall();
+  }, []);
+  return (
+    <Container>
+      <Row>
+        {dogs &&
+          dogs.map((dog) => {
+            return (
+              <Col>
+                <Card style={{ width: "10rem" }}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title>Name: {dog.name}</Card.Title>
+                    <Card.Text></Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+      </Row>
+    </Container>
+  );
+};
 
-            {dogs && dogs.map(dog => {
-                return (
-                    <Col>name:{dog.name}</Col>
-                )
-            })}
-        </Container>
-    )
-}
-
-export default WalkerHome
+export default WalkerHome;
