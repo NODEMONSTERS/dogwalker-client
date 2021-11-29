6eef43003e936bfc46fe4c261dd36e70140f887c
keithro <rodriguez.keith@gmail.com>
2021-11-29 16:41:35 -0600

    Add dog functionality with @ProJaymmer & @dma151

--- src/components/AddDogForm.js
+++ src/components/AddDogForm.js
@@ -1,16 +1,60 @@
 import React, {useState} from 'react';
-
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
 
-const AddDogForm = () => {
+const AddDogForm = (props) => {
   const [show, setShow] = useState(false);
+  const [inputVal, setInputVal] = useState({
+    name: '',
+    breed: '',
+    age: '',
+    picture: '',
+    personality: '',
+    walkLengths: '',
+  })
 
   const handleClose = () => setShow(false);
-  const handleShow = () => {
-    console.log('you are adding a dog')
-    setShow(true)
-  };
+  const handleShow = () => setShow(true);
+
+  const handleInputChange = (e) => {
+    setInputVal({ ...inputVal, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('adding a dog...')
+
+    fetch('http://localhost:4000/dog', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputVal),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.newDog._id);
+
+      fetch(`http://localhost:4000/owner/${props.userId}/addDog`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({id: data.newDog._id})
+      })
+      .then((res) => res.json())
+      .then((owner) => {
+        console.log(owner.data);
+        props.setOwnerInfo(owner.data);
+        handleClose();
+      })
+      .catch(e => console.log(e));
+    })
+    .catch(e => console.log(e))
+
+  }
 
   return (
     <>
@@ -28,20 +72,42 @@ const AddDogForm = () => {
           <Modal.Title>Add a dog</Modal.Title>
         </Modal.Header>
         <Modal.Body>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" name='name' value={inputVal.name} onChange={handleInputChange} placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Breed</Form.Label>
+              <Form.Control type="text" name='breed' value={inputVal.breed} onChange={handleInputChange} placeholder="Enter breed" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Age</Form.Label>
+              <Form.Control type="number" name='age' value={inputVal.age} onChange={handleInputChange} placeholder="Enter age" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Picture</Form.Label>
+              <Form.Control type="text" name='picture' value={inputVal.picture} onChange={handleInputChange} placeholder="Enter image url" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Personality</Form.Label>
+              <Form.Control type="text" name='personality' value={inputVal.personality} onChange={handleInputChange} placeholder="What is your dog's temperment" />
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label>Walk Lengths</Form.Label>
+              <Form.Control type="text" name='walkLengths' value={inputVal.walkLengths} onChange={handleInputChange} placeholder="How long of a walk does your dog prefer" />
+            </Form.Group>
           
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Modal.Body>
-
-
-        {/* <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer> */}
       </Modal>
     </>
   );
--- src/components/OwnerHome.js
+++ src/components/OwnerHome.js
@@ -8,7 +8,6 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 
 const OwnerHome = (props) => {
-  // console.log(props);
 
   // Temp id. Will need to pass props.id from login page
   const userId = "61a3f92faf1b5b76de8c836b";
@@ -25,31 +24,17 @@ const OwnerHome = (props) => {
     fetch(`http://localhost:4000/owner/${userId}`)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.owner);
         setOwnerInfo(data.owner);
       })
       .then(() => {
         fetch(`http://localhost:4000/walker`)
           .then((response) => response.json())
           .then((data) => {
-            console.log(data.allWalkers);
             setWalkerData(data.allWalkers);
           })
       })
       .catch((e) => console.log(e));
   }, []);
-
-
-  
-// Adding a new dog
-  const handleAddDogClick = () => {
-    console.log('you are adding a dog')
-    // 1st API call to create dog
-    // returns dog data (including id) = newDog
-
-    // 2nd API call needs dog id
-    // const url = exampleurl/oqner/98712348/addDog/12352345
-  }
 
   const dogListJSX = ownerInfo.dogs.map((dog) => {
     return <Dog dog={dog} />
@@ -68,8 +53,7 @@ const OwnerHome = (props) => {
       </div>
       <Container>
         <h2>My Dogs</h2>
-        {/* <Button variant="primary" onClick={handleAddDogClick}>Add a Dog</Button> */}
-        <AddDogForm />
+        <AddDogForm userId={userId} setOwnerInfo={setOwnerInfo} />
         <Row>
           {dogListJSX}
         </Row>
