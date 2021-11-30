f84f5957741bd876f3534d19886ab50e164331cc
keithro <rodriguez.keith@gmail.com>
2021-11-30 13:13:37 -0600

    basic styling to owner page

--- src/components/OwnerHome.js
+++ src/components/OwnerHome.js
@@ -4,61 +4,61 @@ import Walker from './Walker';
 import AddDogForm from './AddDogForm';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import './OwnerHome.css';
 
 const OwnerHome = (props) => {
+  const userId = props.userId;
 
-	const userId = props.userId;
+  const [ownerInfo, setOwnerInfo] = useState({
+    name: "",
+    city: "",
+    dogs: [],
+    email: "",
+  });
 
-	const [ownerInfo, setOwnerInfo] = useState({
-		name: '',
-		city: '',
-		dogs: [],
-		email: '',
-	});
+  const [walkerData, setWalkerData] = useState([]);
 
-	const [walkerData, setWalkerData] = useState([]);
+  useEffect(() => {
+    fetch(`https://pacific-wave-42416.herokuapp.com/owner/${userId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setOwnerInfo(data.owner);
+      })
+      .then(() => {
+        fetch(`https://pacific-wave-42416.herokuapp.com/walker`)
+          .then((response) => response.json())
+          .then((data) => {
+            setWalkerData(data.allWalkers);
+          });
+      })
+      .catch((e) => console.log(e));
+  }, []);
 
-	useEffect(() => {
-		fetch(`https://pacific-wave-42416.herokuapp.com/owner/${userId}`)
-			.then((response) => response.json())
-			.then((data) => {
-				setOwnerInfo(data.owner);
-			})
-			.then(() => {
-				fetch(`https://pacific-wave-42416.herokuapp.com/walker`)
-					.then((response) => response.json())
-					.then((data) => {
-						setWalkerData(data.allWalkers);
-					});
-			})
-			.catch((e) => console.log(e));
-	}, []);
+  const dogListJSX = ownerInfo.dogs.map((dog) => {
+    return <Dog dog={dog} setOwnerInfo={setOwnerInfo} ownerInfo={ownerInfo} />;
+  });
+  const walkerList = walkerData.map((walker) => {
+    return <Walker owner={ownerInfo} walker={walker} />;
+  });
 
-	const dogListJSX = ownerInfo.dogs.map((dog) => {
-		return <Dog dog={dog} setOwnerInfo={setOwnerInfo} ownerInfo={ownerInfo} />;
-	});
-	const walkerList = walkerData.map((walker) => {
-		return <Walker owner={ownerInfo} walker={walker} />;
-	});
-
-	return (
-		<Container>
-			<h1>Owner homepage</h1>
-				<div>
-				<h2>Hello {ownerInfo.name}</h2>
-				<h4>{ownerInfo.city}</h4>
-			</div>
-			<Container>
-				<h2>My Dogs</h2>
-				<AddDogForm userId={userId} setOwnerInfo={setOwnerInfo} />
-				<Row>{dogListJSX}</Row>
-			</Container>
-			<Container>
-				<h2>Dog Walkers</h2>
-				<Row>{walkerList}</Row>
-			</Container>
-		</Container>
-	);
+  return (
+    <Container className="owner-container">
+      <h1>Owner homepage</h1>
+      <div className="owner-info">
+        <h2>Hello {ownerInfo.name}</h2>
+        <h4>{ownerInfo.city}</h4>
+      </div>
+      <Container className="dog-container">
+        <h2>My Dogs</h2>
+        <AddDogForm userId={userId} setOwnerInfo={setOwnerInfo} />
+        <Row>{dogListJSX}</Row>
+      </Container>
+      <Container className="walker-container">
+        <h2>Dog Walkers</h2>
+        <Row>{walkerList}</Row>
+      </Container>
+    </Container>
+  );
 };
 
 export default OwnerHome;
