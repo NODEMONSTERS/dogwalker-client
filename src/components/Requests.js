c5c1a75cc212c9d6033ff01f646652fc034b6c2b
Daniel An <mkandaniel@gmail.com>
2021-11-29 21:38:06 -0800

    added requests in the lists

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -10,21 +10,27 @@ const Requests = (props) => {
     const makeApiCall = async () => {
         const res = await fetch(`http://localhost:4000/walker/${props.walker._id}`)
         const json = res.json();
-        console.log(json)
+        setRequests(json.requests)
     };
     makeApiCall()
-  }, []);
+  });
 
   const requestCards = requests.map((request) => {
+      const singleDog = request.dogs.map((dog) => {
+          return(<li>{dog.name}</li>)
+      })
     return (
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
           <Card.Title>Request!</Card.Title>
           <Card.Text>
-            <p>Name:</p>
-            <p>Email:</p>
-            
+            <p>Name:{request.ownerName}</p>
+            <p>Email:{request.ownerEmail}</p>
+            <ul>
+                <p>Dogs:</p>
+                {singleDog}
+            </ul>
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
